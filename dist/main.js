034a7e062bf58e5ad9831e8f8c5d818d30360a67
IvanGameDev1101 <kuznecov_1002@mail.ru>
2024-07-26 01:53:45 +0300

    may be now?

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser from '/npm/phaser@3.55.2/dist/phaser.js';
 import GameScene from '/tgbotgame/dist/scenes/MainScene';
 import ShopScene from '/tgbotgame/dist/scenes/ShopScene';
 import AchievementsScene from '/tgbotgame/dist/scenes/AchievementsScene';
